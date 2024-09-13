b99fcbc8d9de5cf30e1cfd6714fe1bb6a796ad0d
kballadi <kalyan.alladi@neudesic.com>
2024-09-13 13:55:01 +0530

    Slider JS code, Social media font-awesome css changes

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+            showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+            function plusSlides(n) 
+            {
+                console.log("Slide Show begins");
+                showSlides(slideIndex += n);
+            }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("images-carousal");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+            function showSlides(n) 
+            {
+                let i;
+                let slides = document.getElementsByClassName("mySlides");
+                if (n > slides.length) {slideIndex = 1}
+                if (n < 1) {slideIndex = slides.length}
+                for (i = 0; i < slides.length; i++) 
+                {
+                    slides[i].style.display = "none";
+                }
+                slides[slideIndex-1].style.display = "block";
+                dots[slideIndex-1].className += " active";
+                setTimeout(showSlides, 2000); // Change image every 2 seconds
+            }
