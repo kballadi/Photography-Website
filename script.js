efa2d3ad65563b358c2f510ffe5ede980f2328b8
kballadi <kalyan.alladi@neudesic.com>
2024-09-19 11:43:42 +0530

    Adjusted Top Nav bar to fit the contents

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 let slideIndex = 1;
-            showSlides(slideIndex);
+showSlides(slideIndex);
 
-            function plusSlides(n) 
-            {
-                console.log("Slide Show begins");
-                showSlides(slideIndex += n);
-            }
+function plusSlides(n) 
+{
+    console.log("Slide Show begins");
+    showSlides(slideIndex += n);
+}
 
-            function showSlides(n) 
-            {
-                let i;
-                let slides = document.getElementsByClassName("mySlides");
-                if (n > slides.length) {slideIndex = 1}
-                if (n < 1) {slideIndex = slides.length}
-                for (i = 0; i < slides.length; i++) 
-                {
-                    slides[i].style.display = "none";
-                }
-                slides[slideIndex-1].style.display = "block";
-                dots[slideIndex-1].className += " active";
-                setTimeout(showSlides, 2000); // Change image every 2 seconds
-            }
+function showSlides(n) 
+{
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) 
+    {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
